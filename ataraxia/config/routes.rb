7b9de9b6613aa0f89806e745f6366be4da180c08
daniel <gangsterdan@gmail.com>
2017-04-30 20:07:54 -0500

    Add default URL '/' route to home/index

--- ataraxia/config/routes.rb
+++ ataraxia/config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-	
+
+	#default
+	get '/' => 'home#index'
+
   get 'home/index'
 
 	post 'users/create' => 'users#create'
 	get 'users/new' => 'users#new'
 	get 'users' => 'users#index'
 
-	
+
 	post 'properties/create' => 'properties#create'
 	post 'properties/show' => 'properties#show'
 	get 'properties/new' => 'properties#new'
 	get 'properties' => 'properties#index'
 
-	
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
